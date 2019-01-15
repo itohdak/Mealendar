36f60964d3862ad864090700924cc29a311f6785
itohdak <h-ito@jsk.imi.i.u-tokyo.ac.jp>
2019-01-15 15:10:18 +0900

    Initial Commit

---
+++ Mealendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mealendar
+//
+//  Created by Hideaki Ito on 2019/01/15.
+//  Copyright © 2019 Hideaki Ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
